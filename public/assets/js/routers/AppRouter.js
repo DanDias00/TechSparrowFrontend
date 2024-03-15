3d7ccb52b0be5e9d979c755e16e6bb35d198b823
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-03-16 01:16:12 +0530

    router updat

--- public/assets/js/routers/AppRouter.js
+++ public/assets/js/routers/AppRouter.js
@@ -6,7 +6,12 @@ var AppRouter = Backbone.Router.extend({
         'register': 'showRegister', // #register
         'questions': 'showQuestions', // #questions
         'questions/:id': 'showQuestionDetail',
-        'ask': 'showAskQuestion' // #ask
+        'ask': 'showAskQuestion', // #ask
+        'forgot': 'showForgotPassword', // #forgot
+        'reset_password/:id': 'showResetPassword', // #reset_password
+        'email_success': 'showEmailSuccess', // #email_sucess
+        'success': 'showSuccess', // #success
+
 
     },
 
@@ -34,6 +39,22 @@ var AppRouter = Backbone.Router.extend({
     showAskQuestion: function() {
         console.log("Ask question view would be rendered here.");
         new AskQuestionView();
+    },
+    showForgotPassword: function() {
+        console.log("Forgot password view would be rendered here.");
+        new ForgotPasswordView();
+    },
+    showResetPassword: function(id) {
+        console.log("Reset password view would be rendered here.");
+        new ResetPasswordView({id: id});
+    },
+    showEmailSuccess: function() {
+        console.log("Email success view would be rendered here.");
+        new EmailSuccessView();
+    },
+    showSuccess: function() {
+        console.log("Success view would be rendered here.");
+        new SuccessView();
     }
 
 });
