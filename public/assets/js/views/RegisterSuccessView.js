f97f6a4583be8f137e0ef165d68530ea42c5134e
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-04-20 00:08:34 +0530

    Register success view

---
+++ public/assets/js/views/RegisterSuccessView.js
@@ -0,0 +1,38 @@
+// views/SuccessView.js
+var RegisterSuccessView = Backbone.View.extend({
+    el: '#app',
+
+    template: _.template(`
+        <div class="success-wrapper">
+            <div class="success-inner">
+                <h2>Success!</h2>
+                <p>Your have been registered successfully!</p>
+                <div class="success-icon">
+                  
+                    <img src="assets/images/correct.png" alt="Success">
+                </div>
+                <button id="homeButton" class="btn btn-warning mt-3">login</button>
+            </div>
+        </div>
+    `),
+
+    events: {
+        'click #homeButton': 'navigateHome'
+    },
+    initialize: function() {
+      
+        this.render();
+    },
+
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    },
+
+    navigateHome: function() {
+        // Navigate to the home page or route
+        Backbone.history.navigate('login', { trigger: true });
+    }
+});
+
+
