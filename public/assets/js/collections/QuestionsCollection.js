c9f239ed4aa3e62027b6b4bd3150e45f35101edc
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-04-20 00:24:43 +0530

    modify search functionality

--- public/assets/js/collections/QuestionsCollection.js
+++ public/assets/js/collections/QuestionsCollection.js
@@ -1,13 +1,23 @@
 var QuestionsCollection = Backbone.Collection.extend({
     model: QuestionModel,
-    url: 'http://localhost/TechSparrow/index.php/questions_api/questions', // Your API endpoint for fetching questions
+    url: 'http://localhost/TechSparrow/index.php/questions_api/questions', // API endpoint for fetching questions
 
     search: function(query) {
+        var self = this;
         // Update the collection's URL with the search query
         this.url = 'http://localhost/TechSparrow/index.php/questions_api/search?q=' + encodeURIComponent(query);
     
         // Fetch the results and reset the collection with them
-        this.fetch({ reset: true });
+        this.fetch({ 
+            reset: true,
+            error: function(collection, response) {
+                // Handle error
+                if(response.status === 404) {
+                    self.trigger('noResultsFound');     
+                }
+            }
+    
+        });
     },
     
 });
