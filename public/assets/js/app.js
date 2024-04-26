5be2224359cc0acc6068699f1942a146825bcee5
DanDias00 <dandiasabeyesinghe@gmail.com>
2024-04-26 09:12:16 +0530

    NavBar initialization change

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     new AppRouter();
     // Start Backbone history a necessary step for bookmarkable URL's
     Backbone.history.start();
-});
+    console.log('Document ready');
+    // Initialize navbar
+    var sessionData = JSON.parse(localStorage.getItem('session')) || { loggedIn: false };
+    var session = new SessionModel(sessionData);
+    new NavbarView({ model: session });
+    
+}
+);
